2e123470869ea11f7baad076383c91e18295e09e
JiaenLIU <63045662+JiaenLiu@users.noreply.github.com>
2025-02-14 16:41:31 +0800

    refactor ASR module

--- src/ASR/ASR.py
+++ src/ASR/ASR.py
@@ -1,14 +1,21 @@
-from openai import OpenAI
 from pathlib import Path
+from openai import AzureOpenAI
 
-import logging
-import torch
 import stable_whisper
+import torch
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 
-def get_transcript(method, src_srt_path, source_lang, audio_path, client, task_logger, pre_load_asr_model = None):
 
-    is_trans = False # is trans flag 
+def get_transcript(
+    method,
+    src_srt_path,
+    source_lang,
+    audio_path,
+    client,
+    task_logger,
+    pre_load_asr_model=None,
+):
+    is_trans = False  # is trans flag
 
     if not Path.exists(src_srt_path):
         # extract script from audio
@@ -17,52 +24,69 @@ def get_transcript(method, src_srt_path, source_lang, audio_path, client, task_l
         init_prompt = "Hello, welcome to my lecture." if source_lang == "EN" else ""
 
         # process the audio by method
-        if method == "whisper-api": 
-            transcript = get_transcript_whisper_api(audio_path, source_lang, init_prompt, client)
+        if method == "whisper-api":
+            transcript = get_transcript_whisper_api(
+                audio_path, source_lang, init_prompt, client
+            )
             is_trans = True
         elif method == "whisper-large-v3":
             transcript = get_transcript_whisper_large_v3(audio_path, pre_load_asr_model)
             is_trans = True
         elif "stable" in method:
             whisper_model = method.split("-")[2]
-            transcript = get_transcript_stable(audio_path, whisper_model, init_prompt, pre_load_asr_model)
+            transcript = get_transcript_stable(
+                audio_path, whisper_model, init_prompt, pre_load_asr_model
+            )
             is_trans = True
         else:
-            raise RuntimeError(f"unavaliable ASR inference method: {method}")   
-    
+            raise RuntimeError(f"unavaliable ASR inference method: {method}")
+
     # return transcript or None
-    if (is_trans == True):
-        return transcript    
-    else: 
+    if is_trans == True:
+        return transcript
+    else:
         return None
-        
-def get_transcript_whisper_api(audio_path, source_lang, init_prompt, client):
-    with open(audio_path, 'rb') as audio_file:
-        transcript = client.audio.transcriptions.create(model="whisper-1", file=audio_file, response_format="srt", language=source_lang.lower(), prompt=init_prompt)
+
+
+def get_transcript_whisper_api(audio_path, source_lang, init_prompt):
+    client = AzureOpenAI()
+    with open(audio_path, "rb") as audio_file:
+        transcript = client.audio.transcriptions.create(
+            model="whisper-1",
+            file=audio_file,
+            response_format="srt",
+            language=source_lang.lower(),
+            prompt=init_prompt,
+        )
     return transcript
-    
-def get_transcript_stable(audio_path, whisper_model, init_prompt, pre_load_asr_model = None):
+
+
+def get_transcript_stable(
+    audio_path, whisper_model, init_prompt, pre_load_asr_model=None
+):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     if pre_load_asr_model is not None:
         model = pre_load_asr_model
     else:
         model = stable_whisper.load_model(whisper_model, device)
-    transcript = model.transcribe(str(audio_path), regroup=False, initial_prompt=init_prompt)
+    transcript = model.transcribe(
+        str(audio_path), regroup=False, initial_prompt=init_prompt
+    )
     (
-        transcript
-        .split_by_punctuation(['.', '。', '?'])
-        .merge_by_gap(.15, max_words=3)
-        .merge_by_punctuation([' '])
-        .split_by_punctuation(['.', '。', '?'])
+        transcript.split_by_punctuation([".", "。", "?"])
+        .merge_by_gap(0.15, max_words=3)
+        .merge_by_punctuation([" "])
+        .split_by_punctuation([".", "。", "?"])
     )
     transcript = transcript.to_dict()
-    transcript = transcript['segments']
+    transcript = transcript["segments"]
     # after get the transcript, release the gpu resource
     torch.cuda.empty_cache()
 
     return transcript
 
-def get_transcript_whisper_large_v3(audio_path, pre_load_asr_model = None):
+
+def get_transcript_whisper_large_v3(audio_path, pre_load_asr_model=None):
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
     torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
     model_id = "openai/whisper-large-v3"
@@ -70,29 +94,33 @@ def get_transcript_whisper_large_v3(audio_path, pre_load_asr_model = None):
     if pre_load_asr_model is not None:
         model = pre_load_asr_model
     else:
-        model = AutoModelForSpeechSeq2Seq.from_pretrained(model_id, torch_dtype=torch_dtype)
-    
+        model = AutoModelForSpeechSeq2Seq.from_pretrained(
+            model_id, torch_dtype=torch_dtype
+        )
+
     model.to(device)
 
     processor = AutoProcessor.from_pretrained(model_id)
     pipe = pipeline(
-    "automatic-speech-recognition",
-    model=model,
-    tokenizer=processor.tokenizer,
-    feature_extractor=processor.feature_extractor,
-    max_new_tokens=128,
-    chunk_length_s=30,
-    batch_size=16,
-    return_timestamps=True,
-    torch_dtype=torch_dtype,
-    device=device,
+        "automatic-speech-recognition",
+        model=model,
+        tokenizer=processor.tokenizer,
+        feature_extractor=processor.feature_extractor,
+        max_new_tokens=128,
+        chunk_length_s=30,
+        batch_size=16,
+        return_timestamps=True,
+        torch_dtype=torch_dtype,
+        device=device,
     )
 
     transcript_whisper_v3 = pipe(str(audio_path))
 
     # convert format
     transcript = []
-    for i in transcript_whisper_v3['chunks']:
-        transcript.append({'start': i['timestamp'][0], 'end': i['timestamp'][1], 'text':i['text']})
+    for i in transcript_whisper_v3["chunks"]:
+        transcript.append(
+            {"start": i["timestamp"][0], "end": i["timestamp"][1], "text": i["text"]}
+        )
 
     return transcript
