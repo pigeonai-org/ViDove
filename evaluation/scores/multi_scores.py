c33a455f3857bad05ada1a7b1eeb94450f8e9a95
SeleiXi <bzhanthresh@gmail.com>
2025-03-24 00:40:49 +0800

    fix: Update COMET model checkpoint to correct the COMET score

--- evaluation/scores/multi_scores.py
+++ evaluation/scores/multi_scores.py
@@ -7,7 +7,7 @@ class multi_scores:
     def __init__(self, source_lang="en", target_lang="zh", domain="starcraft 2") -> None:
         # 22 is not supported by comet
         # self.comet_model = load_from_checkpoint(download_model("Unbabel/wmt22-comet-da"))
-        self.comet_model = load_from_checkpoint(download_model("Unbabel/wmt20-comet-da"))
+        self.comet_model = load_from_checkpoint(download_model("Unbabel/wmt22-comet-da"))
         self.bleu_model = BLEU(tokenize=target_lang)
         self.LLM_model = LLM_eval.init_evaluator(source_lang=source_lang, target_lang=target_lang, domain=domain)
         # self.score = {}
@@ -29,7 +29,12 @@ class multi_scores:
     def calculate_comet_llm(self, src:str, mt:str, ref:str) -> dict:
         # preprocess the input
         src, mt, ref = self.__preprocess(src, mt, ref)
-        comet_score = self.comet_model.predict([{"src":src, "mt":mt, "ref":ref}], batch_size=8, gpus=0).scores[0]
+        comet_output = self.comet_model.predict([{"src":src, "mt":mt, "ref":ref}], batch_size=1, gpus=0)
+        comet_score = comet_output.scores[0]
+        # comet_score = self.comet_model.predict([{"src":src, "mt":mt, "ref":ref}], batch_size=8, gpus=0).scores[0]
+        
+        # print(f"\n\n\n\n---------------------\n{comet_output}\n----------------------\n\n\n\n\n")
+        
         # bleu_score = self.bleu_model.corpus_score([mt], [ref]).score
         llm_acc, llm_completeness = LLM_eval.evaluate_prediction(src, ref, mt, self.LLM_model)
         return {'comet_score':comet_score, 'llm_score':llm_acc[0], 'llm_explanation': llm_acc[1]}
@@ -93,16 +98,18 @@ class multi_scores:
         return results
 
 if __name__ == "__main__":
-    src = "South Korea playing with the Blue Proto's Probes"
-    src_list = [src,"你好"]
-    mt = "位于对角线的另一个角落  使用蓝色的Proto's Probes"
-    mt_list = [mt,"世界"]
-    ref = " 在对角落里使用蓝色神族探机 他的名字是..."
-    ref_list = [ref,"世界"]
+    src = "The South Korea player is encountering with the Blue Terran's SCV"
+    src_list = [src,"Hello","Second, I recommend greasing the groove, which means several times a day hanging on the bar for about 50 percent of your max hold time. It's all about doing submaximal sets. You want to practice frequently while keeping yourself feeling as fresh as possible. Grease the groove every single day."]
+    mt = "位于对角线的另一个角落  使用蓝色的Terran Probes"
+    mt_list = [mt,"你好","其次,我建议使用“润滑槽”方法,这意味着每天多次在单杠上悬挂,时间约为你最大悬挂时间的50%,这主要是进行低于最大强度的训练,你需要频繁练习,同时尽量保持身体的清新感,每天都要进行“润滑槽”训练."]
+    ref = " 来自南韩的玩家遇到了来自蓝色人族的SCV"
+    ref_list = [ref,"你好","第二，我推荐磨合训练法，单杠训练一天多次，锻炼时间保持在你最长记录的50%，也就是做次强度训练。勤加练习，同时保持精力充沛。每天如是磨合训练。"]
     # print(multi_scores().get_scores(src, mt, ref))
-    # print(multi_scores().calculate_comet_llm(src, mt, ref))
-    # print(multi_scores().calculate_bleu([mt], [[ref]]))
-    print("start")
-    print(multi_scores().calculate_comet_llm_batch(src_list, mt_list, ref_list))
-    print("end")
+    print(multi_scores().calculate_comet_llm(src, mt, ref))
+    print("\n\n\n BLUE ------------------------------------:")
+    print(multi_scores().calculate_bleu(mt_list, ref_list))
+    print("\n\n\n COMET ----------------- LLM ------------------------------------:")
+    for result in multi_scores().calculate_comet_llm_batch(src_list, mt_list, ref_list):
+        print(result)
+    # print(multi_scores().calculate_comet_llm_batch(src_list, mt_list, ref_list))
         
