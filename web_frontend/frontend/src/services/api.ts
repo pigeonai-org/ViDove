9e4e5764a19884054b32f25fcf4f6e0459eae59d
Xudong Xiao <aotuman233@gmail.com>
2025-07-04 19:46:32 -0500

    Update npm to production version

--- web_frontend/frontend/src/services/api.ts
+++ web_frontend/frontend/src/services/api.ts
@@ -16,7 +16,7 @@ import {
   ApiError as ApiErrorInterface
 } from '../types/api';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || `${window.location.protocol}//${window.location.hostname}:8000`;
+const API_BASE_URL = process.env.REACT_APP_API_URL || `${window.location.protocol}//${window.location.hostname}`;
 
 class ApiError extends Error {
   constructor(public status: number, message: string) {
